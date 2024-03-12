f40af28ccc2856907f7b2a23d2d12e7a637325fa
Paulinka26 <268403@student.pwr.edu.pl>
2024-03-12 18:31:48 +0100

    Implementacja metod mnozenia i dzielenia w klasie Calculator

--- src/main/java/org/example/CalculatorLauncher.java
+++ src/main/java/org/example/CalculatorLauncher.java
@@ -11,4 +11,11 @@ public class CalculatorLauncher {
     public int subtract(int a, int b) {
         return a - b;
     }
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }i
 }
