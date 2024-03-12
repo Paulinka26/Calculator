a269be9018a10cc695bb9109cca9bb608c72a433
Paulinka26 <268403@student.pwr.edu.pl>
2024-03-12 18:25:32 +0100

    Implementacja metod dodawania i odejmowania w klasie Calculator

--- src/main/java/org/example/CalculatorLauncher.java
+++ src/main/java/org/example/CalculatorLauncher.java
@@ -4,4 +4,11 @@ public class CalculatorLauncher {
     public static void main(String[] args) {
         System.out.println("Coming soon!");
     }
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
